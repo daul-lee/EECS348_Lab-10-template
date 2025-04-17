c82a4d21d57863ae318335d9a1c38027ecd46e37
Daul Lee <daulplee12624120@gmail.com>
2025-04-16 23:17:22 -0500

    Finalized file inputs

--- main.cpp
+++ main.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
-
+#include <fstream>
+#include <string>
 #include "calculator.hpp"
-
+using namespace::std;
 int main(int argc, char *argv[]) {
-
+    string line;
+    string filename; 
+    cout << "Enter the name of the file: ";
+    cin >> filename;
+    ifstream myfile (filename);
+    if (myfile.is_open()){ 
+        while ( getline (myfile,line) ){ 
+            cout << parse_number(line)<< '\n';
+            cout << parse_number(line) + -123.456 << '\n';
+        }
+        myfile.close();
+    } else cout << "Unable to open file";
+return 0;
 }
